e5e27214e5664661b7ef0e1a37cf44110f051c3c
rooneyc <rooneyc@TER-ZRUQYFNZSII>
2026-01-27 14:46:52 -0800

    add cable factory

--- cable_factory.py
+++ cable_factory.py
@@ -13,3 +13,26 @@ def create_cable(serial_number: str):
     key = serial_number.strip().upper()[1]
     cls, length = CABLE_MAP[key]
     return cls(serial_number, length)
+
+GOLDEN_MAP = {
+    "11796-0312" : (Paradise, 12),
+    "11796-0315": (Paradise, 15),
+    "14131-034": (Paradise, 34),
+    "11989-0315": (Tesla, 15),
+    "11989-0312": (Tesla, 12)
+}
+def create_golden_cable(test_name):
+
+    if not isinstance(test_name, str):
+        raise TypeError("test_name must be a string")
+    if("11796-0312" in test_name):
+        return Paradise("golden", 12)
+    elif("11796-0315" in test_name):
+        return Paradise("golden", 15)
+    elif("14131-034" in test_name):
+        return Paradise("golden", 34)
+    elif("11989-0315" in test_name):
+        return Tesla("golden", 15)
+    elif("11989-0312" in test_name):
+        return Tesla("golden", 12)
+    
